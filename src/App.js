35fa32c53da474dc6bc5f1b37a613900b3769bfc
QMaximillian <quinnlashinsky@gmail.com>
2018-08-10 10:56:17 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React from 'react'
+
+
+class App extends React.Component {
+
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(console.log)
+  }
+  //   .then(astros => this.mapAstros(astros))
+  // }
+  //
+  // mapAstros(astros) {
+  //   console.log(astros)
+  //   const mappedAstros = astros.people.map(astro => {
+  //     return <div>{astro.name}</div>
+  //   })
+  // }
+
+  render(){
+  return(
+    <div></div>
+  )}
+}
+
+export default App
